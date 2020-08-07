93a1523ceae6a4533274b97a8b7ebcfa8ec34ac3
Cool Kornak <cool@coolspages.com>
2020-08-06 21:37:09 -0600

    Update to link

--- src/main/java/maven_bot/commands/Github.java
+++ src/main/java/maven_bot/commands/Github.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class Github {
     public Github(MessageReceivedEvent event) {
         MessageChannel channel = event.getChannel();
-            channel.sendMessage("The bot's repository can be found at: https://github.com/Kulkinz/maven_java_discord_bot")
+            channel.sendMessage("The bot's repository can be found at: https://github.com/Kulkinz/first_no_bot_ics")
                 .queue();
     }
 }
