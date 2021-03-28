1fac19cecdc73809000602836f3560f21ff18198
Cool Kornak <cool@coolspages.com>
2021-03-27 21:30:01 -0600

    Basis for the quote command

---
+++ src/main/java/maven_bot/commands/Quote.java
@@ -0,0 +1,10 @@
+package maven_bot.commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Quote {
+    
+    public Quote(MessageReceivedEvent event, String quote, String author, String context) {
+        
+    }
+}
